f5d52dc9a675b8bb5c75fb99e6e465c57d35cc2a
edenreinfurt <73365705+edenoctocat@users.noreply.github.com>
2021-11-12 17:31:19 +0000

    Completed all parts of assignment

--- Program3.py
+++ Program3.py
@@ -5,4 +5,12 @@ PROGRAM 3
 Define a function second_smallest that takes a list of integers or floats as an argument. The function returns the 2nd smallest number in the list.
 '''
 def second_smallest(lst):
-  pass #delete this line when you start writing your code
+  smallest = lst[0]
+  secsmallest = lst[0]
+  for number in lst:
+    if number < smallest:
+      secsmallest = smallest
+      smallest = number
+    elif number < secsmallest:
+      secsmallest = number
+  return secsmallest
--- Program4.py
+++ Program4.py
@@ -7,4 +7,12 @@ Define a function above_average that takes a list of integers or floats as an ar
 You may define a separate function that finds the average of a list, though you don't have to.
 '''
 def above_average(lst):
-  pass #delete this line when you start writing your code
+  total = 0
+  aboveaveragelst = []
+  for number in lst:
+    total += number
+  average = total/len(lst)
+  for number in lst:
+    if number > average:
+      aboveaveragelst.append(number)
+  return aboveaveragelst
